ed80b88262fa2da39373ad6a357145c67c6ca348
Thomas Thicketman <tomtores@gmail.com>
2024-12-11 21:37:23 +0000

    add source data + transformer app

---
+++ PowerEfficiency/CalculatorApp/FileAccessor.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleApp1.Program;
+
+namespace ConsoleApp1
+{
+    internal class FileAccessor
+    {
+        public static Recipe[] ReadRecipes()
+        {
+            //headers:
+            //Recipe Name	Product	Amount	ProducedIn	Power	Ing1	Am1	Ing2	Am2	Ing3	Am3	Ing4	Am4	Ing5	Am5
+
+            var results = new List<Recipe>();
+            //read from csv, file name items.csv, the columns are <Display Name><Points> 
+            string filePath = "recipes.csv";
+            try
+            {
+                using (var reader = new StreamReader(filePath))
+                {
+                    string? line;
+                    _ = reader.ReadLine();  //skip header row
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        var values = line.Split(','); // Split by comma
+                        results.Add(new Recipe
+                        {
+                            Name = values[0],
+                            Product = values[1],
+                            Amount = decimal.Parse(values[2]),
+                            ProducedIn = values[3],
+                            BuildingPower = decimal.Parse(values[4]),
+                            Ingredients = GetIngredients(values, 5),
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            return results.ToArray();
+        }
+
+        public static void WriteRecipes(Recipe[] recipes)
+        {
+            //headers:
+            //Recipe Name	Product	Amount	ProducedIn	Power	Ing1	Am1	Ing2	Am2	Ing3	Am3	Ing4	Am4	Ing5	Am5
+
+            string filePath = "recipes_out.csv";
+
+            try
+            {
+                using (var writer = new StreamWriter(filePath))
+                {
+                    // Write header
+                    writer.WriteLine("Total Cost,Recipe Name,Product,Amount,ProducedIn,Power,Ing1,Am1,Ing2,Am2,Ing3,Am3,Ing4,Am4,Ing5,Am5");
+
+                    // Write each person
+                    foreach (var recipe in recipes)
+                    {
+                        writer.WriteLine($"{recipe.TotalCost ?? 0},{recipe.Name},{recipe.Product},{recipe.Amount},{recipe.ProducedIn},{recipe.BuildingPower},{ListIngredients(recipe.Ingredients)}");
+                    }
+                }
+
+                Console.WriteLine("CSV written successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+        }
+
+        private static string ListIngredients((string name, decimal amount)[] ingredients)
+        {
+            return string.Join(',', ingredients.Select(i => $"{i.name},{i.amount}"));
+        }
+
+        private static (string name, decimal amount)[] GetIngredients(string[] values, int startIndex)
+        {
+            var ingredientValues = values.Skip(startIndex).ToArray();
+            var results = new List<(string, decimal)>();
+            for (int i = 0; i < ingredientValues.Length; i = i + 2)
+            {
+                if (!string.IsNullOrWhiteSpace(ingredientValues[i]))
+                {
+                    results.Add((ingredientValues[i], decimal.Parse(ingredientValues[i + 1])));
+                }
+            }
+
+            return results.ToArray();
+        }
+
+        public static Item[] ReadItems()
+        {
+            var results = new List<Item>();
+            //read from csv, file name items.csv, the columns are <Display Name><Points> 
+            string filePath = "items.csv";
+            try
+            {
+                using (var reader = new StreamReader(filePath))
+                {
+                    string? line;
+                    _ = reader.ReadLine();  //skip header row
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        var values = line.Split(','); // Split by comma
+                        results.Add(new Item { Name = values[0], Points = int.Parse(values[1]) });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            return results.ToArray();
+        }
+
+        public static void WriteItems(Item[] items)
+        {
+            string filePath = "items_out.csv";
+
+            try
+            {
+                using (var writer = new StreamWriter(filePath))
+                {
+                    // Write header
+                    writer.WriteLine("MW,Item,Recipe,Points");
+
+                    // Write each person
+                    foreach (var item in items)
+                    {
+                        writer.WriteLine($"{item.Cost ?? 0},{item.Name},{item.RecipeName},{item.Points}");
+                    }
+                }
+
+                Console.WriteLine("CSV written successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ PowerEfficiency/CalculatorApp/Program.cs
@@ -0,0 +1,157 @@
+﻿
+
+
+
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reading");
+            var items = FileAccessor.ReadItems();
+            Console.WriteLine($"Loaded {items.Length} items");
+            var recipes = FileAccessor.ReadRecipes();
+            Console.WriteLine($"Loaded {recipes.Length} recipes");
+
+            Iterate(items, recipes, 10000, defaultToZero: true);
+
+            FileAccessor.WriteItems(items);
+            FileAccessor.WriteRecipes(recipes);
+            Console.WriteLine("Done!");
+        }
+
+        private static void Iterate(Item[] items, Recipe[] recipes, int repeats, bool defaultToZero)
+        {
+            //step one - update all item costs
+            //step two - calculate all recipes cost
+            //repeat until max iterations or no change
+
+            if (!defaultToZero)
+            {
+                FixupRecipeStartingPosition(recipes);
+            }
+
+
+            for (int i = 0; i < repeats; i++)
+            {
+                foreach (Recipe recipe in recipes)
+                {
+                    if (!defaultToZero)
+                    {
+                        var newRecipeCost = CalculateRecipeCost(recipe, items);
+                        if (newRecipeCost != null)
+                        {
+                            //Debug.Assert(!(recipe.TotalCost != null && newRecipeCost > recipe.TotalCost));
+                            if (newRecipeCost < (recipe.TotalCost ?? 0) && newRecipeCost >= 0)
+                            {
+                                recipe.TotalCost = newRecipeCost;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        recipe.TotalCost = CalculateRecipeCost2(recipe, items);
+                    }
+                }
+
+                foreach (Item item in items)
+                {
+                    var newCost = CalculateCost(item, recipes);
+                    if (newCost.cost != null)
+                    {
+                        item._previousCost = item.Cost;
+                        (item.Cost, item.RecipeName) = newCost;
+                    }
+                }
+
+                if (items.Any(i => i.Cost != i._previousCost))
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine($"Calculation stalled at {i}");
+                    break;
+                }
+            }
+        }
+
+        private static void FixupRecipeStartingPosition(Recipe[] recipes)
+        {
+            //to prevent endless loops, initialize all ecipe totals to extremely large values - they will be pruned off when actual recipe costs are calculated
+            foreach(Recipe recipe in recipes)
+            {
+                recipe.TotalCost = 999999;
+            }
+        }
+
+        private static decimal? CalculateRecipeCost(Recipe recipe, Item[] items)
+        {
+            var cost = recipe.BuildingPower;
+
+            foreach (var ingredient in recipe.Ingredients)
+            {
+                var itemCost = items.Single(i => i.Name == ingredient.name).Cost;
+                if (itemCost == null)
+                {
+                    return null;
+                }
+
+                cost += ingredient.amount * itemCost.Value;
+            }
+
+            return cost / recipe.Amount;
+        }
+
+        private static decimal? CalculateRecipeCost2(Recipe recipe, Item[] items)
+        {
+            var cost = recipe.BuildingPower;
+
+            foreach (var ingredient in recipe.Ingredients)
+            {
+                var itemCost = items.Single(i => i.Name == ingredient.name).Cost;
+               
+                cost += ingredient.amount * itemCost ?? 0;
+            }
+
+            return cost / recipe.Amount;
+        }
+
+        private static (decimal? cost, string recipe) CalculateCost(Item item, Recipe[] recipes)
+        {
+            var validRecipes = recipes.Where(r => r.Product == item.Name && r.TotalCost != null);
+            var cheapest = validRecipes.OrderBy(vr => vr.TotalCost).FirstOrDefault();
+            if (cheapest != null)
+            {
+                return (cheapest.TotalCost, cheapest.Name);
+            }
+
+            return (null, null);
+        }
+
+        public class Item
+        {
+            public string Name { get; set; }
+            public int Points { get; set; }
+            public string RecipeName { get; set; }
+
+            public decimal? Cost { get; set; }   //in MW/unit/minute
+
+            public decimal? _previousCost { get; set; }
+        }
+
+        public class Recipe
+        {
+            public string Name { get; set; }
+            public string Product { get; internal set; }
+            public decimal Amount { get; internal set; }
+            public string ProducedIn { get; internal set; }
+            public decimal BuildingPower { get; internal set; }
+            public (string name, decimal amount)[] Ingredients { get; internal set; }
+            public decimal? TotalCost { get; internal set; }
+        }
+    }
+}
